ce216a53e74099c906b9449c5ecd09e5702a12df
withgod <noname@withgod.jp>
2020-12-02 20:01:04 +0900

    update

--- update.sh
+++ update.sh
@@ -5,7 +5,9 @@ set -eo pipefail
 release=$(lsb_release -cs)
 version=$1
 
+echo 1
 sudo apt-fast update
+echo 2
 
 # sudo apt-get purge 'php*'
 if [ $release == 'xenial' ]
@@ -13,10 +15,12 @@ then
 	sudo apt-fast purge 'libssl1.1'
 	sudo apt-fast purge 'postgresql*'
 fi
+echo 3
 sudo apt-fast install -y libcurl4-nss-dev libjpeg-dev re2c libxml2-dev \
 	libtidy-dev libxslt-dev libmcrypt-dev libreadline-dev libfreetype6-dev \
 	libonig-dev zlib1g-dev libzip-dev mysql-client
 
+echo 4
 sudo ln -s /usr/include/x86_64-linux-gnu/curl /usr/local/include/curl
 
 
